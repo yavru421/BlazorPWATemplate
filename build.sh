dfb2e5835b16ea91b23ab9dbfee785f49a9f6310
J.Dondlinger <dondlingergeneralcontracting@gmail.com>
2026-05-26 14:43:51 -0500

    Add Cloudflare Pages Linux build script to install .NET SDK 10.0

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Download the .NET SDK installation script
+curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
+chmod +x dotnet-install.sh
+
+# Install .NET SDK 10.0 locally
+./dotnet-install.sh -c 10.0 -InstallDir ./dotnet
+
+# Publish the application using the local SDK installation
+./dotnet/dotnet publish -c Release -o output
